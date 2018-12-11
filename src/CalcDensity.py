f6ed63f7fc20968dbb883446fb5c6ec91e16391b
Erik Forsberg <erikforsberg95@hotmail.com>
2018-12-11 16:48:37 +0100

    calc dist

--- src/CalcDensity.py
+++ src/CalcDensity.py
@@ -1,6 +1,16 @@
 import numpy as np
 from scipy.spatial import distance_matrix
 
-def calc_density(xs, ys)
+def calc_density(xs, ys):
 
-positions = 
+npts = len(xs)
+dist__mat = np.zeros(npts)
+counter = 0
+
+for i in range(npts):
+    for j in range(i):
+        counter += np.sqrt((xs(i)-xs(j))^2 + (ys(i)-ys(j))^2)
+
+counter = counter/(npts*(npts-1))
+
+return counter
